fbbd0e8dcde6883f874a9b2c35b568211f7dba02
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 15:52:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Determines if a number is prime
+ * @author Cole Girling
  */
 public class Main {
 
@@ -9,7 +11,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Which number would you like to check?");
+    int userNumber = input.nextInt();
+
+    int count = 1;
+
+    while(userNumber % count == 1){
+      count = count + 1;
+      if(count == userNumber){
+        break;
+      }
+    }
+    System.out.println(userNumber);
   }
 }
